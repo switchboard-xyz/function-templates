69b77000a3f518d0a17bcf8d4674b899ae7d5b7d
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-08-13 21:27:30 -0600

    fix: gha

--- testing.sh
+++ testing.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+old_pwd="${PWD}"
 for d in ./templates/*; do
-  if [[ -d "${d}" ]]; then
-      dirname="${d%/}"
-      template_name=$(basename "${dirname}")
-      zip -r "${template_name}.zip" "${dirname}"
-      gh release upload ${{ steps.release.outputs.upload_url }} ${dirname}.zip
-  fi
+    if [[ -d "${d}" ]]; then
+        dirname="${d%/}"
+        template_name=$(basename "${dirname}")
+        cd "${dirname}" && zip -r "${old_pwd}/${template_name}.zip" .
+        cd "${old_pwd}" || exit
+        gh release upload ${{ steps.release.outputs.tag_name }} "${template_name}.zip"
+    fi
 done
