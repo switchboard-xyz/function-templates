887fd9603158eeb008cd28faca7719cc73a2d1dc
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-08-13 19:24:13 -0600

    fix: gha

--- .github/actions/build-release/index.js
+++ .github/actions/build-release/index.js
@@ -1,38 +1,13 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const fs = require("fs");
-const zip = require("zip-a-folder");
-
-async function buildRelease(template, uploadUrl) {
-  const inputDirectory = `./templates/${template}`;
-  const outputZipFile = `./.artifacts/${template}.zip`;
-
-  await zip.zip(inputDirectory, outputZipFile);
-
-  const uploadAssetResponse = await github.repos.uploadReleaseAsset({
-    url: uploadUrl,
-    // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
-    headers: {
-      "content-type": "application/tgz",
-      "content-length": fs.statSync(outputZipFile).size,
-    },
-    name: template,
-    file: fs.readFileSync(outputZipFile),
-  });
-
-  // Get the browser_download_url for the uploaded release asset from the response
-  const {
-    data: { browser_download_url: browserDownloadUrl },
-  } = uploadAssetResponse;
-
-  // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-  return browserDownloadUrl;
-}
+import core from "@actions/core";
+import github from "@actions/github";
+import fs from "fs";
+import path from "path";
+import zip from "zip-a-folder";
 
 async function run() {
-  // const github = new github.GitHub(
-  //   core.getInput("gh-token", { required: true })
-  // );
+  const octokit = github.getOctokit(
+    core.getInput("gh-token", { required: true })
+  );
 
   const tagName = core.getInput("tag-name", { required: true });
   console.log(`TagName: ${tagName}`);
@@ -47,9 +22,31 @@ async function run() {
 
   fs.mkdirSync(".artifacts");
 
-  const artifacts = await Promise.all(
-    templates.map((t) => buildRelease(t, uploadUrl))
-  ).filter((t) => Boolean(t));
+  const artifacts = [];
+  for await (const template of templates) {
+    const inputDirectory = `./templates/${template}`;
+    const outputZipFile = `./.artifacts/${template}.zip`;
+
+    await zip.zip(inputDirectory, outputZipFile);
+
+    const uploadAssetResponse = await octokit.repos.uploadReleaseAsset({
+      url: uploadUrl,
+      // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
+      headers: {
+        "content-type": "application/tgz",
+        "content-length": fs.statSync(outputZipFile).size,
+      },
+      name: template,
+      file: fs.readFileSync(outputZipFile),
+    });
+
+    // Get the browser_download_url for the uploaded release asset from the response
+    const {
+      data: { browser_download_url: browserDownloadUrl },
+    } = uploadAssetResponse;
+
+    artifacts.push(browserDownloadUrl);
+  }
 
   core.setOutput("artifacts", artifacts);
 
